e5db2ff774ea11793afa69b3a1304ec7abecf061
Giuseppe Cardillo <37176275+dnafinder@users.noreply.github.com>
2025-12-05 15:52:02 +0100

    Update polybius.m
    Refine polybius core
    
    - Accept char and string scalar input
    - Clarify documentation for 5x5 (I/J) and 6x6 (A–Z + 0–9) modes
    - Explicitly separate text vs numeric branches
    - Strengthen numeric input validation
    - Add GitHub Crypto reference in header

--- polybius.m
+++ polybius.m
@@ -5,8 +5,8 @@ function out=polybius(x,ms)
 % Democleitus, and perfected by the Ancient Greek historian and scholar
 % Polybius, for fractionating plaintext characters so that they can be
 % represented by a smaller set of symbols. The original square used the
-% Greek alphabet, but it can be used with any other alphabet. 
-% With the modern English alphabet, this is the typical form: 
+% Greek alphabet, but it can be used with any other alphabet.
+% With the modern English alphabet, this is the typical form:
 %
 %    1   2   3   4   5
 % 1  A   B   C   D   E
@@ -20,7 +20,7 @@ function out=polybius(x,ms)
 % in a square, it is rounded down to the next lowest square number by
 % combining two letters, usually I and J (Polybius had no such problem
 % because the Greek alphabet has 24 letters). Alternatively, the ten digits
-% could be added, and 36 characters would be put into a 6 × 6 grid.
+% could be added, and 36 characters would be put into a 6 x 6 grid.
 %
 %    1   2   3   4   5   6
 % 1  A   B   C   D   E   F
@@ -33,35 +33,36 @@ function out=polybius(x,ms)
 % Syntax: 	out=polybius(x,ms)
 %
 %     Input:
-%           x - It can be a characters array or a numbers array. In first
-%           case it will encoded; in the second case it will decoded. 
-%           ms - this parameter can assume only two values: 
-%                   5 to use a 5x5 Polybius square
-%                   6 to use a 6x6 Polybius square
+%           x  - It can be a character array, a string scalar, or a numeric array.
+%                In the first case it will be encoded; in the second case it
+%                will be decoded.
+%           ms - this parameter can assume only two values:
+%                   5 to use a 5x5 Polybius square (I/J combined)
+%                   6 to use a 6x6 Polybius square (A-Z and 0-9)
 %     Output:
 %           out - It is a structure
-%           out.plain = the plain text
+%           out.plain = the plain text (uppercase)
 %           out.ms = the size of Polybius Square
 %           out.encrypted = the coded text
 %
 % Examples:
-% 
+%
 % out=polybius('Hide the gold into the tree stump',5)
-% 
-% out = 
-% 
+%
+% out =
+%
 %   struct with fields:
-% 
+%
 %         plain: 'HIDE THE GOLD INTO THE TREE STUMP'
 %            ms: 5
 %     encrypted: [23 24 14 15 44 23 15 22 34 31 14 24 33 44 34 44 23 15 44 42 15 15 43 44 45 32 35]
 %
 % out=polybius([23 24 14 15 44 23 15 22 34 31 14 24 33 44 34 44 23 15 44 42 15 15 43 44 45 32 35],5)
 %
-% out = 
-% 
+% out =
+%
 %   struct with fields:
-% 
+%
 %     encrypted: [23 24 14 15 44 23 15 22 34 31 14 24 33 44 34 44 23 15 44 42 15 15 43 44 45 32 35]
 %            ms: 5
 %         plain: 'HIDETHEGOLDINTOTHETREESTUMP'
@@ -71,58 +72,86 @@ function out=polybius(x,ms)
 %
 %           Created by Giuseppe Cardillo
 %           giuseppe.cardillo.75@gmail.com
+%           GitHub (Crypto): https://github.com/dnafinder/crypto
 
-assert(ms==5 || ms==6,'Polybius matrix can be 5x5 or 6x6')
+assert(isnumeric(ms) && isscalar(ms) && ismember(ms,[5 6]), ...
+    'Polybius matrix can be 5x5 or 6x6')
+
 % rearrange ASCII codes into a square matrix
 switch ms
     case 5
-        %PS=ASCII CODES FOR [ABCDEFGHIKLMNOPQRSTUVWXYZ]
-        PS=reshape([65:1:73 75:1:90],5,5)'; %5x5 Polybius Square
+        % PS = ASCII codes for [ABCDEFGHIKLMNOPQRSTUVWXYZ]
+        PS=reshape([65:1:73 75:1:90],5,5)'; % 5x5 Polybius Square
     case 6
-        %PS=ASCII CODES FOR [ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789]
-        PS=reshape([65:1:90 48:1:57],6,6)'; %6x6 Polybius Square
+        % PS = ASCII codes for [ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789]
+        PS=reshape([65:1:90 48:1:57],6,6)'; % 6x6 Polybius Square
 end
 
-if ischar(x) %encrypt
-    % Set all letters in uppercase and convert into ASCII Code.
+if isstring(x) && isscalar(x)
+    x = char(x);
+end
+
+if ischar(x) % encrypt
     out.plain=upper(x);
-    text=double(out.plain);
     out.ms=ms;
+
+    % Set all letters in uppercase and convert into ASCII Code.
+    text=double(out.plain);
+
     switch ms
         case 5
-            % Erase all characters that are not into the range 65 - 90;
+            % Erase all characters that are not into the range 65 - 90
             text(text<65 | text>90)=[];
             % Convert J (ASCII code 74) into I (ASCII code 73)
             text(text==74)=73;
         case 6
-            % ASCII codes for Uppercase letters ranges between 65 and 90;
-            % ASCII codes for digits ranges between 48 and 57;
+            % ASCII codes for uppercase letters are between 65 and 90
+            % ASCII codes for digits are between 48 and 57
             % Erase all ASCII codes between 57 and 65; below 48 and above 90
             text(text>57 & text<65)=[];
             text(text<48 | text>90)=[];
     end
+
     % Find the index of each characters into Polybius square
     [~,locb]=ismember(text,PS);
     % transform index into subscripts
     [I,J]=ind2sub([ms,ms],locb);
-    % Combine subcripts
+    % Combine subscripts
     out.encrypted=I.*10+J;
-else
+
+elseif isnumeric(x) % decrypt
+    assert(isreal(x) && all(isfinite(x(:))), ...
+        'Input numeric array must be real and finite.')
+    assert(all(mod(x(:),1)==0), ...
+        'Input numeric array must contain integers only.')
+
     switch ms
         case 5
-            assert(all(ismember(x,[11:1:15 21:1:25 31:1:35 41:1:45 51:1:55])),'This array can''t be decoded using a 5x5 Polybius matrix')
+            allowed = [11:15 21:25 31:35 41:45 51:55];
+            assert(all(ismember(x(:),allowed)), ...
+                'This array can''t be decoded using a 5x5 Polybius matrix')
         case 6
-            assert(all(ismember(x,[11:1:16 21:1:26 31:1:36 41:1:46 51:1:56 61:1:66])),'This array can''t be decoded using a 6x6 Polybius matrix')
+            allowed = [11:16 21:26 31:36 41:46 51:56 61:66];
+            assert(all(ismember(x(:),allowed)), ...
+                'This array can''t be decoded using a 6x6 Polybius matrix')
     end
+
     out.encrypted=x;
     out.ms=ms;
+
     % From each two-digits number:
     % the first digit is the row
     I=fix(x./10);
     % the second is the column
     J=x-I.*10;
-    % trasform subscripts into index
-    Ind=sub2ind([ms,ms],I,J); clear I J
+
+    % transform subscripts into index
+    Ind=sub2ind([ms,ms],I,J);
+
     % take ASCII codes from Polybius square and transform them into letters
     out.plain=char(PS(Ind));
+
+else
+    error('Input x must be a character array, a string scalar, or a numeric array.')
+end
 end
